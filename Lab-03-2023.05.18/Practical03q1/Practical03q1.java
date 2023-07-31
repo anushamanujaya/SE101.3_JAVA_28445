e2d5cf49f1ebb8ad1471191f18c6a3f41fac82df
anushamanujaya <anushmanuja@gmail.com>
2023-07-31 23:12:51 +0530

    Add practical 03

---
+++ Lab-03-2023.05.18/Practical03q1/Practical03q1.java
@@ -0,0 +1,17 @@
+package com.mycompany.practical03q1;
+
+public class Practical03q1 {
+
+    public static void main(String[] args) {
+         Encapsulation e1=new Encapsulation();
+        e1.setName("Dilshani");
+        e1.setAge(23);
+        e1.setSalary(250000);
+        
+        System.out.println("Name: "+e1.getName());
+        System.out.println("Age:"+e1.getAge());
+        System.out.println("Salary:"+e1.getSalary());
+
+    
+    }
+}
---
+++ Lab-03-2023.05.18/Practical03q2/Q2.java
@@ -0,0 +1,42 @@
+package com.mycompany.practical03q2;
+public class Q2 {
+    private String empName;
+    private float basicSalary, bonus;
+    
+    
+    public void setEmpName(String empName)
+    {
+        this.empName=empName;
+    }
+    public String getEmpName()
+    {
+        return empName;
+    }
+    
+    public void setBasicSalary(float basicSalary)
+    {
+        this.basicSalary=basicSalary;
+    }
+    public float getBasicSalary()
+    {
+        return basicSalary;
+    }
+    
+    public Q2(float bonus)
+    {
+        this.bonus=bonus;
+    }
+    public void display()
+    {
+        System.out.println("Bonus: "+bonus);
+    }
+    
+    private float bonusAmount;
+    public void calBonusAmount()
+    {
+        bonusAmount=bonus+basicSalary;
+        System.out.println("Bonus Amount: "+bonusAmount);
+    }
+}  
+
+
