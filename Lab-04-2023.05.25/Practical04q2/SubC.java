965f84437dc2056d9995c5f5c459b022c6e38fdd
anushamanujaya <anushmanuja@gmail.com>
2023-07-31 23:14:43 +0530

    Add practical 04

---
+++ Lab-04-2023.05.25/Practical04q2/SubC.java
@@ -0,0 +1,6 @@
+package com.mycompany.practical04q2;
+public class SubC extends SupperB {
+    void triple () {x=x+3;} 
+    void quadruple () {x=x*4;} 
+    
+}
---
+++ Lab-04-2023.05.25/Practical04q2/SupperB.java
@@ -0,0 +1,9 @@
+package com.mycompany.practical04q2;
+class SupperB {
+    int x;
+    void setIt (int n) { x=n;}
+    void increase () { x=x+1;}
+    void triple () {x=x*3;};
+    int returnIt () {return x;
+}
+}
