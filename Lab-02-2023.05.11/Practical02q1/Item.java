73041d2d233126693ae43cfbbbf0acab36ac202b
anushamanujaya <anushmanuja@gmail.com>
2023-07-31 23:09:08 +0530

    Add practical 02

---
+++ Lab-02-2023.05.11/Practical02q1/Item.java
@@ -0,0 +1,32 @@
+
+package com.mycompany.practical02;
+public class Item {
+    private int location;
+    private String description;
+    public void setValues(int l, String d)
+    {
+        location=l;
+        description=d;
+    }
+    
+    
+    public void setlocation(int location)
+    {
+        this.location=location;
+    }
+    public int getlocation()
+    {
+        return location;
+    }
+    
+    public void setdescription(String description)
+    {
+        this.description=description;
+    }
+    
+    public void display1()
+    {
+        System.out.println("Location1: "+location);
+        System.out.println("Description1: "+description);
+}
+}
---
+++ Lab-02-2023.05.11/Practical02q1/Practical02.java
@@ -0,0 +1,14 @@
+package com.mycompany.practical02;
+public class Practical02 {
+
+    public static void main(String[] args) {
+       Monster m1=new Monster();
+       m1.setlocation(25);
+       m1.setdescription("Galle");
+       m1.setData(41, "Poddala");
+       m1.display1();
+       m1.display2();
+
+}
+    }
+
